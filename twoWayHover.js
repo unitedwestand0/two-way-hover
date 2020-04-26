ee67838ac870d9d804002fbbc00c252a60f4a828
PioneerQueens <46530671+unitedwestand0@users.noreply.github.com>
2020-04-26 18:55:25 -0500

    CSS and JS files are located in their own folders this is just for easy access to the code for editing

---
+++ twoWayHover.js
@@ -0,0 +1,62 @@
+/** 
+--------------------------------------------------------------
+Let’s attach an event handler both from when the list items are 
+hovered onto and hovered off. When those things happen, through 
+the data attribute we’ll figure out which SVG state is relevant, 
+then add classes to both.
+--------------------------------------------------------------
+*/
+
+var wordStates = document.querySelectorAll(".list-of-states li");
+var svgStates = document.querySelectorAll("#states > *");
+
+function removeAllOn() {
+  wordStates.forEach(function(el) {
+    el.classList.remove("on");
+  });
+  svgStates.forEach(function(el) {
+    el.classList.remove("on");
+  });
+}
+
+function addOnFromList(el) {
+  var stateCode = el.getAttribute("data-state");
+  var svgState = document.querySelector("#" + stateCode);
+  el.classList.add("on");
+  svgState.classList.add("on");
+}
+
+function addOnFromState(el) {
+  var stateId = el.getAttribute("id");
+  var wordState = document.querySelector("[data-state='" + stateId + "']");
+  el.classList.add("on");
+  wordState.classList.add("on");
+}
+
+wordStates.forEach(function(el) {
+  el.addEventListener("mouseenter", function() {
+    addOnFromList(el);
+  });
+  el.addEventListener("mouseleave", function() {
+     removeAllOn();
+  });
+  
+  el.addEventListener("touchstart", function() {
+    removeAllOn();
+    addOnFromList(el);
+  });
+});
+
+svgStates.forEach(function(el) {
+  el.addEventListener("mouseenter", function() {
+    addOnFromState(el);
+  });
+  el.addEventListener("mouseleave", function() {
+     removeAllOn();
+  });
+  
+  el.addEventListener("touchstart", function() {
+    removeAllOn();
+    addOnFromState(el);
+  });
+});
